e8b1eeebde32b69c20326e5b8b2d4970a953f3ce
valepost91 <valerio.postiglione@gmail.com>
2014-01-20 18:23:11 +0100

    added libraries and a first connection to the database with simple request

---
+++ TPSIG/database/Utils.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author sylvain
+ */
+public class Utils {
+    private static Connection connection;
+    private static String login = "monLogin";
+    private static String password = "monMDP";
+    
+    
+    public static Connection getConnection() {
+        if (connection != null) return connection;
+        return createConnection();
+    }
+    
+    private static Connection createConnection() {
+	Connection conn = null;
+	try {
+            Logger.getLogger(Utils.class.getName()).log(Level.INFO, "Loading PostgreSQL driver...");
+	    Class.forName("org.postgresql.Driver");
+            Logger.getLogger(Utils.class.getName()).log(Level.INFO, "Trying to connect to Grenoble database...");
+	    String url = "jdbc:postgresql://ensibd.imag.fr:5432/osm";
+	    conn = DriverManager.getConnection(url, login, password);
+            Logger.getLogger(Utils.class.getName()).log(Level.INFO, "Connected.");
+	    ((org.postgresql.PGConnection) conn).addDataType("geometry",Class.forName("org.postgis.PGgeometry"));
+	    ((org.postgresql.PGConnection) conn).addDataType("box3d",Class.forName("org.postgis.PGbox3d"));
+	} catch (SQLException e) {
+            Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, "Connection error: {0}", e.toString());
+	} catch (ClassNotFoundException e) {
+            Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, "Error while loading postgis extensions: {0}", e.toString());
+	}
+	return conn;	
+    }
+    
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
--- TPSIG/src/tpsig/TPSIG.java
+++ TPSIG/src/tpsig/TPSIG.java
@@ -4,6 +4,13 @@
  */
 package tpsig;
 
+import static database.Utils.getConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.postgis.PGgeometry;
+
 /**
  *
  * @author postiglv
@@ -13,7 +20,20 @@ public class TPSIG {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // TODO code application logic here
+        Connection connection;
+        
+        connection=getConnection();
+        
+        System.out.print("Enter request: ");
+        String input = System.console().readLine();
+        
+        PreparedStatement stmt =  connection.prepareStatement("Select * from nodes where name like '%"+input+"'");
+        ResultSet res = stmt.executeQuery();
+        while (res.next()) {
+            System.out.println("colonne 1 = " + res.getInt(1) + "; colonne 2 = " + ((PGgeometry) res.getObject(2)).getGeometry());
+        }
+
     }
 }
