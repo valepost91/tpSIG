a64fa5653e04edad095436b5db86fcda90a18e78
Thibaud Spieser <spiesert@ensibm.imag.fr>
2014-01-21 09:41:33 +0100

    Update method for getting input, as System.Console is not usable with IDE

--- TPSIG/src/tpsig/TPSIG.java
+++ TPSIG/src/tpsig/TPSIG.java
@@ -5,6 +5,9 @@
 package tpsig;
 
 import static database.Utils.getConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -23,13 +26,20 @@ public class TPSIG {
     public static void main(String[] args) throws SQLException {
         // TODO code application logic here
         Connection connection;
-        
-        connection=getConnection();
-        
+
+        connection = getConnection();
+
         System.out.print("Enter request: ");
-        String input = System.console().readLine();
-        
-        PreparedStatement stmt =  connection.prepareStatement("Select * from nodes where name like '%"+input+"'");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = null;
+        try {
+            input = br.readLine();
+        } catch (IOException e) {
+            System.out.println("IO error trying to read your input");
+            System.exit(1);
+        }
+
+        PreparedStatement stmt = connection.prepareStatement("Select * from nodes where name like '%" + input + "'");
         ResultSet res = stmt.executeQuery();
         while (res.next()) {
             System.out.println("colonne 1 = " + res.getInt(1) + "; colonne 2 = " + ((PGgeometry) res.getObject(2)).getGeometry());
