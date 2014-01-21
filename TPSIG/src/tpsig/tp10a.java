37b5034d97dffd6f08d938b29cdac418738ae132
Thibaud Spieser <spiesert@ensibm.imag.fr>
2014-01-21 11:14:35 +0100

    Draw ways from Grenoble

--- TPSIG/src/tpsig/tp10a.java
+++ TPSIG/src/tpsig/tp10a.java
@@ -6,7 +6,9 @@ package tpsig;
 
 import static database.Utils.getConnection;
 import geoexplorer.gui.GeoMainFrame;
+import geoexplorer.gui.LineString;
 import geoexplorer.gui.MapPanel;
+import geoexplorer.gui.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -27,7 +29,7 @@ public class tp10a {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws SQLException, InterruptedException {
-        MapPanel map = new MapPanel(5.75, 45.15, 1.3);
+        MapPanel map = new MapPanel(5.75, 45.15, 0.1);
         GeoMainFrame mainFrame = new GeoMainFrame("test",map);
 
         // TODO code application logic here
@@ -36,15 +38,19 @@ public class tp10a {
         connection = getConnection();
         
         // TODO : try to set the geometry as a parameter
-        PreparedStatement stmt = connection.prepareStatement("SELECT linestring FROM ways WHERE ST_Intersects(bbox,ST_SetSRID(ST_MakeBox2D(ST_Point(5.7,45.1),ST_Point(5.8,45.2)),4326));");
+        PreparedStatement stmt = connection.prepareStatement("SELECT linestring FROM ways WHERE exist(tags,'highway') AND ST_Intersects(bbox,ST_SetSRID(ST_MakeBox2D(ST_Point(5.7,45.1),ST_Point(5.8,45.2)),4326));");
         ResultSet res = stmt.executeQuery();
         while (res.next()) {
             Geometry geom = ((PGgeometry) res.getObject(1)).getGeometry();
             int nb = geom.numPoints();
-            //Point[] points = new Point[nb];
+            LineString l = new LineString();
+            org.postgis.Point p;
             for(int i = 0;i<nb;i++) {
-                //points[i] = geom.getPoint(i);
+                p = geom.getPoint(i);
+                l.addPoint(new Point(p.x,p.y));
             }
+            map.addPrimitive(l);
         }
+        
     }
 }
